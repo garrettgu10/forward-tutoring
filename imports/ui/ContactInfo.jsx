52e904001a921bc85c9a672c4472ced821b2996d
Garrett Gu <garrettgu777@gmail.com>
2017-07-28 22:13:26 -0500

    adds contact info stub

---
+++ imports/ui/ContactInfo.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class ContactInfo extends Component {
+
+  constructor(props){
+    super(props);
+    this.state= {
+      email: "",
+      skype: ""
+    }
+  }
+
+  render() {
+    return (
+      <div>testing123</div>
+    )
+  }
+}
