9619d7240067de2197ed972b78fe5044c9ed6f88
Garrett Gu <garrettgu777@gmail.com>
2017-08-02 22:13:40 -0500

    here have more code

--- imports/ui/consistent/TutorsList.jsx
+++ imports/ui/consistent/TutorsList.jsx
@@ -30,6 +30,14 @@ Array.prototype.intersect_sorted = function(b){ //finds intersection of two sort
   return intersect_sorted(this, b);
 }
 
+Array.prototype.shuffle = function() { //shuffles array
+  for(var i = this.length-1; i > 0; i--){
+    var j = Math.round(i * Math.random());
+    [this[i], this[j]] = [this[j], this[i]];
+  }
+  return this;
+}
+
 String.prototype.colorCode = function() {
   var hash = md5(this);
 
@@ -37,10 +45,32 @@ String.prototype.colorCode = function() {
 };
 
 class TutorView extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      chosenTime: 0
+    }
+  }
+
+  handleExpand() {
+    this.props.handleFocus();
+  }
+
+  getChosenTime() {
+    return this.state.chosenTime;
+  }
+
+  handleChosenTimeChanged(event, value) {
+    this.setState({
+      chosenTime: value
+    })
+  }
+
   render() {
     var {tutor, times} = this.props;
     return (
-      <Card>
+      <Card
+        expanded={this.props.expanded}>
         <CardHeader
           title={tutor.profile.fullName}
           subtitle={"Username: " + tutor.username}
@@ -48,12 +78,19 @@ class TutorView extends Component {
             <Avatar backgroundColor={'#'+tutor.username.colorCode()}>
               {tutor.username.charAt(0).toUpperCase()}
             </Avatar>
-          }>
+          }
+          onTouchTap={this.handleExpand.bind(this)}
+          style={{cursor: 'pointer'}}
+          showExpandableButton={true}>
         </CardHeader>
         <CardText
-          style={{paddingTop: '0'}}>
+          style={{paddingTop: '0'}}
+          expandable={true}>
           <div>{tutor.tutorProfile.description}</div>
-          <RadioButtonGroup name="times">
+          <RadioButtonGroup
+            name="times"
+            valueSelected={this.state.chosenTime}
+            onChange={this.handleChosenTimeChanged.bind(this)}>
             {times.intersect_sorted(tutor.tutorProfile.times).map((time) => {
               var day = Math.floor(time/5);
               var hour = time%5 + 5 + this.props.dateOffset;
@@ -74,6 +111,16 @@ class TutorView extends Component {
 class TutorsList extends Component {
   constructor(props) {
     super(props);
+    this.tutorViews = [];
+    this.state = {
+      focusedTutor: 0
+    }
+  }
+
+  handleChangeFocusedTutor(tutorIndex) {
+    this.setState({
+      focusedTutor: tutorIndex
+    })
   }
 
   render() {
@@ -87,8 +134,15 @@ class TutorsList extends Component {
 
     return (
       <div>
-        {this.props.tutors.map((tutor) => (
-          <TutorView dateOffset={this.props.dateOffset} key={tutor._id} tutor={tutor} times={this.props.times} />
+        {this.props.tutors.map((tutor, index) => (
+          <TutorView
+            ref={(ref) => {this.tutorViews[index] = ref;}}
+            dateOffset={this.props.dateOffset}
+            key={tutor._id}
+            tutor={tutor}
+            times={this.props.times}
+            expanded={index === this.state.focusedTutor}
+            handleFocus={this.handleChangeFocusedTutor.bind(this, index)} />
         ))}
       </div>
     )
@@ -98,7 +152,7 @@ class TutorsList extends Component {
 export default createContainer((props) => {
   var subscription = Meteor.subscribe('user.tutorsByTimes', props.times);
   return {
-    tutors: Users.find({role: 1, "tutorProfile.times": {$in: props.times}}).fetch(),
+    tutors: Users.find({role: 1, "tutorProfile.times": {$in: props.times}}, {reactive: false}).fetch().shuffle(), //shuffle tutors to give an equal opportunity
     ready: subscription.ready()
   }
 }, TutorsList);
