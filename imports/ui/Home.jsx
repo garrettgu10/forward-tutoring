134494cea32bcc7a9e4b0531868a72b4e3e375c7
Garrett Gu <garrettgu777@gmail.com>
2017-08-12 00:50:37 -0500

    home page styles

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -15,30 +15,32 @@ export default function Home(props){
   return(
     <div>
       <div className="home_container" style={containerStyles}>
-        <img className="home_logo" src="/forward_tutoring_logo.jpg" />
-        <h1 className="home_title">Free, high-quality tutoring</h1>
-        <h2 className="home_subtitle">Brought to you by the Texas Academy of Mathematics and Science</h2>
-        <div style={{height: '20px'}}></div>
-        {props.loggedIn?
-          <div>
-            <Link to="/forum">
-              <RaisedButton className="home_button" label="Ask a question" />
-            </Link>
-            <Link to="/consistent">
-              <RaisedButton className="home_button" label="My consistent tutor" />
-            </Link>
-          </div>
-          :
-          <div>
-            <Link to="/register">
-              <RaisedButton className="home_button" label="Sign up" />
-            </Link>
-            <Link to="/login">
-              <RaisedButton className="home_button" label="Log in" />
-            </Link>
-          </div>
-        }
-        <div style={{height: '100px'}}></div>
+        <div className="masthead-container">
+          <img className="home_logo" src="/forward_tutoring_logo.jpg" />
+          <h1 className="home_title">Free, high-quality tutoring</h1>
+          <h2 className="home_subtitle">Brought to you by the Texas Academy of Mathematics and Science</h2>
+          <div style={{height: '20px'}}></div>
+          {props.loggedIn?
+            <div>
+              <Link to="/forum">
+                <RaisedButton className="home_button" label="Ask a question" />
+              </Link>
+              <Link to="/consistent">
+                <RaisedButton className="home_button" label="My consistent tutor" />
+              </Link>
+            </div>
+            :
+            <div>
+              <Link to="/register">
+                <RaisedButton className="home_button" label="Sign up" />
+              </Link>
+              <Link to="/login">
+                <RaisedButton className="home_button" label="Log in" />
+              </Link>
+            </div>
+          }
+          <div style={{height: '100px'}}></div>
+        </div>
       </div>
       <div style={{textAlign: 'center', marginTop: '20px'}}>More info coming soon!</div>
     </div>
