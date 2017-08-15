c18d083c10c24ea49b7c84646853fbbc10e6524e
Garrett Gu <garrettgu777@gmail.com>
2017-08-15 00:42:26 -0500

    more code

--- imports/api/schools.js
+++ imports/api/schools.js
@@ -6,6 +6,17 @@ export const Schools = new Mongo.Collection('schools');
 
 if(Meteor.isServer) {
   Meteor.publish('schools', () => {
-    
+    return Schools.find({});
   })
 }
+
+Meteor.methods({
+  'schools.insert'(name, key) {
+    check(name, String);
+    check(key, String);
+    
+    if(name.length === 0) {
+
+    }
+  }
+})
