8fa076083368cdca1bd2243336b33eecbc75cde4
Ryan Chhong <ryanchhong@gmail.com>
2018-06-02 20:49:00 -0500

    Added requested changes (removal of beta, more options for subjects

--- imports/constants/constants.js
+++ imports/constants/constants.js
@@ -2,7 +2,7 @@
  * Created by garrettgu on 7/19/17.
  */
 export const Roles = ['student', 'tutor', 'admin'];
-export const Subjects = ['math', 'science', 'english', 'other'];
+export const Subjects = ['Math', 'Science', 'English', 'SAT', 'ACT', 'SAT Subject Tests', 'Other'];
 export const WaysToSortPosts = ['Latest update', 'Date of creation'];
 export const PostStatusFilters = ['Answered only', 'Unanswered only', 'All'];
 export const Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
